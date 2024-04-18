8443794c6303e410e02f9f089a0cd0c0b996a038
John Waithaka <johnwaruiwaithaka@gmail.com>
2024-04-18 11:07:38 +0300

    control lab reviewed - success

--- index.js
+++ index.js
@@ -1,64 +1,35 @@
 
-  // Write your code here!
-  
-    //this is where we can use conditionals given our argument, someValue
-    //don't forget to return whatever the result is!
 
-function scuberGreetingForFeet(feet) {
+//this is where we can use conditionals given our argument, someValue
+ //don't forget to return whatever the result is!
+
+function scuberGreetingForFeet (feet) {
   if (feet <= 400) {
-      return 'This one is on me!';
+    return 'This one is on me!';
+  } else if (feet <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (2000 < feet && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
   } else {
-      // give other answer
+    return 'No can do.';
   }
 }
 
-
-function scuberGreetingForFeet(feet) {
-  if (feet <= 400) {
-    return "This one is on me!";
-       } else if (feet > 400 && feet <= 2000) {
-          return "That will be twenty bucks.";
-           } else if (feet > 2000 && feet <= 2500) {
-              return "I will gladly take your thirty bucks.";
-             } else {
-              return 'No can do.';}
-}
-
-
-function ternaryCheckCity(city){
+// Write your code here!
+function ternaryCheckCity (city) {
   // Write your code here!
-function ternaryCheckCity() {
-if (city = NYC){
-  return "Ok, sounds good.";
-   } else return "No go."
-}
-}
-
-
-function ternaryCheckCity(city) {
   return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-// main if statement for tip
-//   function switchOnCharmFromTip(tip){
-// if (tip = generous) {
-//   return "Thank you so much.";
-// }
-//   }
-  
-  // Function definition
-  function switchOnCharmFromTip(tip) {
-      // Using a switch statement to handle different cases
-      switch (tip) {
-          case "generous":
-              return "Thank you so much.";
-          case "not as generous":
-              return "Thank you.";
-          default:
-              return "Bye.";
-      }
+function switchOnCharmFromTip (tip) {
+   // Function definition
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
   }
 }
